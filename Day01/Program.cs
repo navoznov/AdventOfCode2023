139f398fed895cc70f37dd07dd1bd52923bbeca2
ivan <ivan.navoznov@gmail.com>
2023-12-01 12:08:58 -0300

    init + Day 01 part 1

---
+++ Day01/Program.cs
@@ -0,0 +1,33 @@
+﻿var lines = File.ReadAllLines("input.txt");
+var digits = Enumerable.Range(0,10).Select(x=> (char)('0' + x)).ToArray();
+var sum = lines.Select(GetNumber).Sum();
+Console.WriteLine(sum);
+
+int GetNumber(string line)
+{
+    var result = 0;
+    var chars = line.ToCharArray();
+    
+    foreach (var t in chars)
+    {
+        if (digits.Contains(t))
+        {
+            result = 10 * (t - '0');
+            break;
+        }
+    }
+
+    for (var i = chars.Length - 1; i >= 0; i--)
+    {
+        if (!digits.Contains(chars[i]))
+        {
+            continue;
+        }
+
+        result += chars[i] - '0';
+        break;
+    }
+
+    return result;
+}
+Console.WriteLine("Hello, World!");
