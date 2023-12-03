37f7243d8d6daa9d8678e44609d3b4c45042aae8
ivan <ivan.navoznov@gmail.com>
2023-12-03 13:27:03 -0300

    Day 02 part 2

--- Day02/Game.cs
+++ Day02/Game.cs
@@ -11,4 +11,16 @@ internal record Game(int Id, Dictionary<Color, int>[] States)
     {
         return state.All(ci => ci.Value <= allCubes[ci.Key]);
     }
+
+    public Dictionary<Color, int> GetMinimumState()
+    {
+        return Enum.GetValues<Color>()
+            .Select(color => new { Color = color, Count = GetMaxCount(color) })
+            .ToDictionary(x => x.Color, x => x.Count);
+    }
+
+    private int GetMaxCount(Color color)
+    {
+        return States.Max(state => state.TryGetValue(color, out var value) ? value : 0);
+    }
 }
--- Day02/Program.cs
+++ Day02/Program.cs
@@ -4,7 +4,7 @@ using State = System.Collections.Generic.Dictionary<Color, int>;
 
 var lines = File.ReadAllLines("input.txt");
 var gameParser = new GameParser();
-var games = lines.Select(line => gameParser.Parse(line));
+var games = lines.Select(line => gameParser.Parse(line)).ToArray();
 
 var state = new State
 {
@@ -13,6 +13,11 @@ var state = new State
     { Color.Blue, 14 },
 };
 
-var part1result = games.Where(g => g.IsValidForCubes(state)).Sum(g => g.Id);
-Console.WriteLine(part1result);
-return;
+var part1Result = games.Where(g => g.IsValidForCubes(state)).Sum(g => g.Id);
+Console.WriteLine(part1Result);
+
+var part2Result = games
+    .Sum(g=>g.GetMinimumState()
+        .Select(x=>x.Value)
+        .Aggregate(1, (x, i) => x*i));
+Console.WriteLine(part2Result);
