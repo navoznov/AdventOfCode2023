49b6ff712d34b947449bae8be552d64367a2025f
ivan <ivan.navoznov@gmail.com>
2023-12-09 10:35:06 -0300

    Day08 part 2

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -1,28 +1,56 @@
-﻿using Day08;
+﻿using System.Diagnostics;
+using Day08;
 
 var lines = File.ReadAllLines("input.txt");
 var inputParser = new InputParser();
 var (directions, nodes) = inputParser.Parse(lines);
 
+// Part 1
 var startNodeName = "AAA";
 var finishNodeName = "ZZZ";
+var stepsCountPart1 = GetStepsCount(nodes[startNodeName], s => s == "ZZZ");
+Console.WriteLine(stepsCountPart1);
+Console.WriteLine();
 
-var currentNode = nodes[startNodeName];
-var stepsCounter = 0;
-while (true)
+// Part 2
+var startNodes = nodes.Values.Where(n => n.Name.Last() == 'A').ToArray();
+var steps = startNodes
+    .Select(node => GetStepsCount(node , s => s.Last() == 'Z'))
+    .ToArray();
+var result2 = FindLeastCommonMultiple(steps);
+Console.WriteLine(result2);
+
+long GetStepsCount(Node startNode, Func<string, bool> checkReachingFinish)
 {
-    var direction = directions[stepsCounter % directions.Length];
-    stepsCounter++;
-    
-    var nextNodeName = direction == 'L' ? currentNode.Left : currentNode.Right;
-    if (nextNodeName == finishNodeName)
+    var currentNode = startNode;
+    var stepsCounter = 0;
+    while (true)
     {
-        break;
-    }
+        var direction = directions[stepsCounter % directions.Length];
+        var nextNodeName = direction == 'L' ? currentNode.Left : currentNode.Right;
     
-    currentNode = nodes[nextNodeName];
+        currentNode = nodes[nextNodeName];
+        stepsCounter++;
+        if (checkReachingFinish(currentNode.Name))
+        {
+            return stepsCounter;
+        }
+    }
 }
 
-Console.WriteLine(stepsCounter);
+long FindLeastCommonMultiple(long[] numbers)
+{
+    return numbers.Aggregate(LCM2);
+}
+
+long GCD2(long a, long b)
+{
+    return b == 0 ? a : GCD2(b, a % b);
+}
+
+long LCM2(long a, long b)
+{
+    return (a / GCD2(a, b)) * b;
+}
 
 record Node(string Name, string Left, string Right);
