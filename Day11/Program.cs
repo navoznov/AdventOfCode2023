6a463d72e1ef81614778ccb79566cabdea1fad6e
ivan <ivan.navoznov@gmail.com>
2023-12-11 22:56:55 -0300

    Day11 part 2

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -3,20 +3,46 @@ using Day11;
 
 var lines = File.ReadAllLines("input.txt");
 var map = new MapParser().Parse(lines);
-var mapPrinter = new MapPrinter();
-Console.WriteLine(map.ToString(mapPrinter.Print, false));
 
-var newMap = new MapTransformer().TransformEmptyLines(map);
-
-var galaxyPoints = newMap.GetAllItems()
+var emptyRowIndexes = GetEmptyLineIndexes(map.YSize, map.GetAllInRow);
+var emptyColumnIndexes = GetEmptyLineIndexes(map.XSize, map.GetAllInColumn);
+var galaxyPoints = map.GetAllItems()
     .Where(item => item.Value == Cell.Galaxy)
     .Select(vp => vp.Point)
     .ToArray();
 var galaxyPairs = GetGalaxyPairs(galaxyPoints).ToArray();
-Console.WriteLine(galaxyPairs.Sum(p => ManhattanDistance(p.first, p.second)));
+
+// Part 1
+var sum1 = galaxyPairs.Sum(gp => CalcDistancesSum(gp, emptyRowIndexes, emptyColumnIndexes, 2));
+Console.WriteLine(sum1);
+
+// Part 2
+var sum2 = galaxyPairs.Sum(gp => CalcDistancesSum(gp, emptyRowIndexes, emptyColumnIndexes, 1_000_000));
+Console.WriteLine(sum2);
 
 
-IEnumerable<(Point first, Point second)> GetGalaxyPairs(Point[] points)
+long CalcDistancesSum((Point First, Point Second) galaxyPair, HashSet<int> emptyRowIndexes,
+    HashSet<int> emptyColumnIndexes, int emptyLineWeight)
+{
+    var (first, second) = galaxyPair;
+    var manhattanDistance = ManhattanDistance(first, second);
+    var (x1, x2) = (int.Min(first.X, second.X), int.Max(first.X, second.X));
+    var (y1, y2) = (int.Min(first.Y, second.Y), int.Max(first.Y, second.Y));
+    var emptyLinesCount = emptyColumnIndexes.Count(x => x > x1 && x < x2)
+                          + emptyRowIndexes.Count(y => y > y1 && y < y2);
+    return manhattanDistance + (emptyLinesCount * (emptyLineWeight - 1));
+}
+
+HashSet<int> GetEmptyLineIndexes(int size, Func<int, IEnumerable<ValuePoint<Cell>>> itemsGetter)
+{
+    return Enumerable.Range(0, size)
+        .Select(i => new { Points = itemsGetter(i), Index = i })
+        .Where(x => x.Points.All(v => v.Value == Cell.Space))
+        .Select(x => x.Index)
+        .ToHashSet();
+}
+
+IEnumerable<(Point First, Point Second)> GetGalaxyPairs(Point[] points)
 {
     for (var i = 0; i < points.Length - 1; i++)
     {
@@ -26,6 +52,7 @@ IEnumerable<(Point first, Point second)> GetGalaxyPairs(Point[] points)
         }
     }
 }
+
 
 static long ManhattanDistance(Point a, Point b)
     => Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
