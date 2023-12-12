e48b6911560941b519ccddceb0879912456123fe
ivan <ivan.navoznov@gmail.com>
2023-12-11 22:34:25 -0300

    Day11 part 1 refactoring

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -6,35 +6,15 @@ var map = new MapParser().Parse(lines);
 var mapPrinter = new MapPrinter();
 Console.WriteLine(map.ToString(mapPrinter.Print, false));
 
-Map2d<Cell> newMap = map;
-newMap = new MapTransformer().TransformEmptyLines(map);
-Console.WriteLine(newMap.ToString(mapPrinter.Print, false));
-
-// .#...
-// ....#
-// #....
+var newMap = new MapTransformer().TransformEmptyLines(map);
 
 var galaxyPoints = newMap.GetAllItems()
     .Where(item => item.Value == Cell.Galaxy)
     .Select(vp => vp.Point)
     .ToArray();
-
-Console.WriteLine($"Galaxies count is {galaxyPoints.Length}");
-Console.WriteLine();
-
 var galaxyPairs = GetGalaxyPairs(galaxyPoints).ToArray();
+Console.WriteLine(galaxyPairs.Sum(p => ManhattanDistance(p.first, p.second)));
 
-var aStar = new AStar();
-Func<Cell, bool> isEmpty = x => x == Cell.Space;
-var distances = new List<long>();
-foreach (var tuple in galaxyPairs)
-{
-    Console.WriteLine(distances.Count + " " + galaxyPairs.Length);    
-    var distance = ManhattanDistance(tuple.first, tuple.second);
-    distances.Add(distance);
-}
-
-Console.WriteLine(distances.Sum());         // 9727458- high
 
 IEnumerable<(Point first, Point second)> GetGalaxyPairs(Point[] points)
 {
@@ -47,5 +27,5 @@ IEnumerable<(Point first, Point second)> GetGalaxyPairs(Point[] points)
     }
 }
 
-    static long ManhattanDistance(Point a, Point b) 
-        => Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
+static long ManhattanDistance(Point a, Point b)
+    => Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
